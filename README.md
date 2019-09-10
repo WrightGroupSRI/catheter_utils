82953464173d28765130a87812d1b4ba1877df75
Sebastian Ferguson <ferguson@sri.utoronto.ca>
2019-09-10 11:00:20 -0400

    Fixup setup.py so that requirements will be installed.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Catheter Utils
 
 This project contains utils related to our catheter tracking projects.
+
+## Tests
+
+Can run with the command `python -m unittest discover tests`.
--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name="catheter_utils", version="0.1.0", packages=find_packages())
+with open("requirements.txt") as req_file:
+    requirements = req_file.read().splitlines()
+
+setup(
+    name="catheter_utils",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=requirements,
+)
