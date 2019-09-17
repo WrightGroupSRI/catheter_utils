a832f6e171ac29e3fae2b6254219b8027c50320e
Sebastian Ferguson <ferguson@sri.utoronto.ca>
2019-09-17 11:25:12 -0400

    Do we even need requirements.txt?

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-with open("requirements.txt") as req_file:
-    requirements = req_file.read().splitlines()
-
 setup(
     name="catheter_utils",
     version="0.1.0",
     packages=["catheter_utils"],
-    install_requires=requirements,
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
 )
